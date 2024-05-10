3aeee683ccd224c4165c2a676a8f2dd980c207e8
Md. Nasim Reza <61214781+nasimit2020@users.noreply.github.com>
2024-05-10 11:29:30 +0600

    Home page Hero section done

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import { Button } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button variant="contained">Text</Button>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 };
 
